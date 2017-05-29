bdc774d8fe6358f349da904c6249a2939b43c4ae
Vohumana <david.linden.james@gmail.com>
2017-05-29 14:41:22 -0700

    Somewhat working version, still needs arrays and sub objects

--- main.go
+++ main.go
@@ -1,22 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
-	"os"
+	"io/ioutil"
 )
 
 func main() {
-	jsonFile, err := os.Open("simple.json")
+	fileData, err := ioutil.ReadFile("simple.json")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer jsonFile.Close()
-	
-	fmt.Println("JsonFile Opened")
+
+	fmt.Println(string(fileData))
 
 	// Parse JSON
-	
-	// Enumerate the interface for all the objects
+	jsonObject := make(map[string]interface{})
+	err = json.Unmarshal(fileData, &jsonObject)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	// Enumerate the interface for all the objects
+	structs := []string{}
+	fmt.Println(jsonObject)
+	for key,value := range jsonObject {
+		valueType := ""
+
+		// TODO: Add arrays and subobjects
+		switch value.(type) {
+			case string:
+				valueType = "string"
+
+			case float64:
+				valueType = "float64"
+
+			case bool:
+				valueType = "bool"
+
+			default:
+				fmt.Printf("Don't know how to parse %v\n", key)
+				continue
+		}
+
+		structs = append(structs, fmt.Sprintf("%v %v `json:%v`", key, valueType, key))		
+	}
+
+	for _,v := range structs {
+		fmt.Println(v)
+	}
 }
